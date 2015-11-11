59d3ace261b1c19ecb2f3d2e198de8b0c74768c8
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2015-11-11 12:46:10 +0100

    pub pres test

---
+++ t/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Function Shipping', 
+     subtitle: 'An overview of approaches', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'clipart/favicon.ico', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Jean-Philippe Bernardy', 
+     company: 'Seagate', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: '', 
+   }]
+};
